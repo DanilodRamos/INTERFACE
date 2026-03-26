987331b4947a10d8f13093bca5c00df7a3a1b147
DanilodeRamos <116719740+DanilodRamos@users.noreply.github.com>
2026-03-26 10:48:36 -0300

    Duas interface
    para que não de o problema do diamante usa-se duas interfaces

---
+++ src/devices/ConcreteScanner.java
@@ -0,0 +1,23 @@
+package devices;
+
+public class ConcreteScanner extends Device implements Printer {
+
+	public ConcreteScanner(String serialNumber) {
+		super(serialNumber);
+	}
+
+	@Override
+	public void processDoc(String doc) {
+		System.out.println("Printer processing: " + doc);
+	}
+
+	@Override
+	public void print(String doc) {
+		System.out.println("Printing: " + doc);
+	}
+
+	public String scan() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
