272e4c1346bed03135e52017b7654405883bb67b
DanilodeRamos <116719740+DanilodRamos@users.noreply.github.com>
2026-03-30 10:21:18 -0300

    comparando objetos

--- src/application/Program5.java
+++ src/application/Program5.java
@@ -1,10 +1,38 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import entities.Employee;
+
 public class Program5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Employee> list = new ArrayList<>();
+		String path = "C:\\temp\\in.txt";
+		
+		// ler arquivo guardando na lista
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String employeeCsv = br.readLine();
+			
+			while (employeeCsv != null) {
+				// recorta pra ler name e salary com vetor
+				String[] fields = employeeCsv.split(",");
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+				employeeCsv = br.readLine();
+				
+			} // operaçao padrao pra ordena coleçao
+			Collections.sort(list);
+			for (Employee emp : list) {
+				System.out.println(emp.getName() + ", " + emp.getSalary());
+			} // exceçao
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
 
 }
